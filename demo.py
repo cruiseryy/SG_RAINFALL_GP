bf4033c529e336e8c6681841781f9eb993ec6b81
cruiseryy <xp53@cornell.edu>
2023-06-20 14:16:59 +0800

    demo v0.1 done

--- demo.py
+++ demo.py
@@ -9,6 +9,8 @@ from cartopy.feature import NaturalEarthFeature
 import cartopy.io.shapereader as shpreader
 import matplotlib.ticker as mticker
 
+from matplotlib import gridspec
+
 
 class gp:
     def __init__(self,
@@ -61,30 +63,47 @@ class gp:
     
     def validation(self):
         xx = self.wrf_sta[0::12, :] + self.wrf_sta[1::12, :]
-        yy = self.rain_sta[0::12, :] + self.rain_sta[1::12, :]
+        fxx = self.rain_sta[0::12, :] + self.rain_sta[1::12, :]
         mu_x = np.mean(xx, axis = 0)
         # Kxx = np.cov(xx.T, ddof=1)
         
-        y_est = np.zeros(yy.shape)
+        fig, ax = plt.subplots(figsize=[20, 18], nrows = 4, ncols = 4)
+        # the usage of y kinda confusing here
+        f_est = np.zeros(fxx.shape)
+
         for j in range(xx.shape[1]):
             txx = np.delete(xx, obj = j, axis = 1)
             t_mu = np.delete(mu_x, obj = j)
-            tyy = np.delete(yy, obj = j, axis = 1)
+            tfxx = np.delete(fxx, obj = j, axis = 1)
             kxx = np.cov(txx.T, ddof = 1)
             kxy = np.cov(xx[:,j].T, txx.T, ddof = 1)[:1, 1:]
             kyy = np.cov(xx[:,j], ddof = 1)
-            y, _ = self.gp_fit(fxx = tyy, mu_fx = t_mu, kxx = kxx, kxy = kxy, mu_fy = mu_x[j], kyy = kyy)
-            KGE = self.kge(true_ = yy[:, j], fit = y)
-            KGE2 = self.kge(true_ = yy[:, j], fit = xx[:, j])
+            fit_y, _ = self.gp_fit(fxx = tfxx, mu_fx = t_mu, kxx = kxx, kxy = kxy, mu_fy = mu_x[j], kyy = kyy)
+            KGE = self.kge(true_ = fxx[:, j], fit = fit_y)
+            KGE2 = self.kge(true_ = fxx[:, j], fit = xx[:, j])
 
-            print('the corrcoef for station {} is {:.2f} ({:.2f}), the KGE is {:.2f} ({:.2f})'.format(j, np.corrcoef(y, yy[:,j])[0, 1], np.corrcoef(xx[:, j], yy[:,j])[0, 1], KGE, KGE2))
+            print('the corrcoef for station {} is {:.2f} ({:.2f}), the KGE is {:.2f} ({:.2f})'.format(j, np.corrcoef(fit_y, fxx[:,j])[0, 1], np.corrcoef(xx[:, j], fxx[:,j])[0, 1], KGE, KGE2))
 
-            y_est[:, j] = y
+            f_est[:, j] = fit_y
+            
+            ri, ci = j // 4, j % 4
+            ax[ri][ci].scatter(fxx[:,j], fit_y)
+            tmpmin = min([np.min(fxx[:,j]), np.min(fit_y)])
+            tmpmax = max([np.max(fxx[:,j]), np.max(fit_y)])
+            ax[ri][ci].plot([tmpmin, tmpmax], [tmpmin, tmpmax], 'k--')
+            ax[ri][ci].set_xlabel('obs rainfall [mm]')
+            ax[ri][ci].set_ylabel('est rainfall [mm]')
+            ax[ri][ci].set_title('(' + chr(ord('a') + j) + ')' + ' KGE = {:.2f} ({:.2f})'.format(KGE, KGE2))
+        
+        ax[3][2].axis('off')
+        ax[3][3].axis('off')
+        plt.tight_layout()
+        fig.savefig('validation.pdf')
         pause = 1
         return
     
     
-    def uncertainty(self):
+    def interpolate(self, plot = 0):
         idx = 0
         mp = np.zeros([2, 120*160])
         yy = np.zeros([40, 120*160])
@@ -113,28 +132,35 @@ class gp:
                 yy[:, idx] = tyy 
                 mp[:, idx] = [i, j]
                 idx += 1
+
         mp = mp[:, :idx].astype(int)
         yy = yy[:, :idx]
 
-        fig, ax = plt.subplots(nrows = 1, ncols = 3, figsize=[24,7], subplot_kw={'projection': crs.PlateCarree()})
-        tmphigh = np.max([np.mean(self.rain_wrf, axis=0), np.mean(fyy, axis=0)])
-        self.map_plotter(ax[0], data = np.mean(self.rain_wrf, axis=0), show_sta = 1, color_high = -1)
-        ax[0].set_title('(a)')
-        self.map_plotter(ax[1], data = np.mean(fyy, axis=0), show_sta = 1, color_high = -1)
-        ax[1].set_title('(b)')
-        self.map_plotter(ax[2], data = np.sqrt(ycov), show_sta = 1)
-        ax[2].set_title('(c)')
-        plt.tight_layout()
-        plt.savefig('comp_shrink.pdf')
+        self.rainfall_interp = fyy
+        self.rainfall_var = ycov
+
+        if plot == 1:
+            fig, ax = plt.subplots(nrows = 1, ncols = 3, figsize=[24,7], subplot_kw={'projection': crs.PlateCarree()})
+            tmphigh = np.max([np.mean(self.rain_wrf, axis=0), np.mean(fyy, axis=0)])
+            self.map_plotter(ax[0], data = np.mean(self.rain_wrf, axis=0), show_sta = 1, color_high = -1)
+            ax[0].set_title('(a)')
+            self.map_plotter(ax[1], data = np.mean(fyy, axis=0), show_sta = 1, color_high = -1)
+            ax[1].set_title('(b)')
+            self.map_plotter(ax[2], data = np.sqrt(ycov), show_sta = 1)
+            ax[2].set_title('(c)')
+            plt.tight_layout()
+            plt.savefig('comp_new.pdf')
         pause = 1
 
         return
-    # def slice_(self, data, buffer = 5):
-    #     data_del = np.delete(data, np.s_[0:buffer], axis = 0)
-    #     data_del = np.delete(data_del, np.s_[-3*buffer:], axis = 0)
-    #     data_del = np.delete(data_del, np.s_[0:2*buffer], axis = 1)
-    #     data_del = np.delete(data_del, np.s_[-buffer:], axis = 1)
-    #     return data_del
+    
+    def slice_(self, data, buffer = 5):
+        n = len(data.shape)
+        data_del = np.delete(data, np.s_[0:buffer], axis = n-2)
+        data_del = np.delete(data_del, np.s_[-3*buffer:], axis = n-2)
+        data_del = np.delete(data_del, np.s_[0:2*buffer], axis = n-1)
+        data_del = np.delete(data_del, np.s_[-buffer:], axis = n-1)
+        return data_del
 
     def map_plotter(self, ax, data, show_sta = 0, color_high = -1):
         self.coastline.plot(ax=ax, facecolor='none', edgecolor='black', linewidth=1)
@@ -149,7 +175,7 @@ class gp:
         if show_sta:
             sta_map = ax.scatter(self.wrf_loc[:,0], self.wrf_loc[:,1], s = 50, facecolors='k', marker='D')
         ax.set_extent([103.58, 104.12, 1.153, 1.502], crs=crs.PlateCarree())
-        cbar = plt.colorbar(basemap, ax=ax, orientation='vertical', shrink=.5)
+        cbar = plt.colorbar(basemap, ax=ax, orientation='vertical', shrink=1)
         cbar.set_label('Rainfall [mm]')
         # cbar.mappable.set_clim(vmin=0, vmax=600) 
         gl = ax.gridlines(crs=crs.PlateCarree(), draw_labels=True, linewidth=1.5, color='gray', alpha=0.5, linestyle='--')
@@ -160,11 +186,47 @@ class gp:
         # ax.set_title(tit)
         pause = 1
         return
+    
+    def hovmoller_diagram(self, ax1, ax2, data):
+        tmplon = self.slice_(self.lon, buffer = 10)
+        tmplat = self.slice_(self.lat, buffer = 10)
+        tmpdata = self.slice_(data, buffer = 10)
+        time = np.array([range(1981, 2021, 1)])
+        lon = np.linspace(np.min(tmplon), np.max(tmplon), tmplon.shape[1])
+        X_mesh, Y_mesh = np.meshgrid(lon, time)
+        Z = np.mean(tmpdata, axis = 1)
+        # fig, ax = plt.subplots()
+        basemap = ax2.contourf(X_mesh, Y_mesh, Z)
+        ax2.set_xlabel('longitude')
+        ax2.set_ylabel('time')
+        ax2.yaxis.get_ticklocs(minor=True)
+        ax2.minorticks_on() 
+        cbar = plt.colorbar(basemap, ax=ax2, orientation='horizontal')
+        cbar.set_label('rainfall [mm]')
+        ax1.plot(lon, np.mean(Z, axis = 0))
+        ax1.set_xlim(left = np.min(lon), right = np.max(lon))
+        ax1.set_ylabel('rainfall [mm]')
+        plt.setp(ax1.get_xticklabels(), visible=False)
+        # fig.savefig('hov_diagram.pdf')
+        pause = 1
+
+        return
 
 
 if __name__ == '__main__':
+    
+    pause = 1
     tmp = gp()
+    # tmp.validation()
     # fig, ax = plt.subplots(figsize=[20,15], subplot_kw={'projection': crs.PlateCarree()})
     # tmp.map_plotter(ax = ax, data = tmp.rain_wrf[0,:,:], show_sta = 1)
     # tmp.validation()
-    tmp.uncertainty()
+    tmp.interpolate()
+    fig, ax = plt.subplots(figsize=(8, 6), nrows=2, ncols=2, height_ratios=[1, 4]) 
+    tmp.hovmoller_diagram(ax1 = ax[0,0], ax2 = ax[1,0], data = tmp.rainfall_interp)
+    ax[0,0].set_title('(a)')
+    tmp.hovmoller_diagram(ax1 = ax[0,1], ax2 = ax[1,1], data = tmp.rain_wrf[0::12, :, :] + tmp.rain_wrf[0::12, :, :])
+    ax[0,1].set_title('(b)')
+    plt.tight_layout()
+    fig.subplots_adjust(hspace=0)
+    fig.savefig('hov_diag.pdf')
